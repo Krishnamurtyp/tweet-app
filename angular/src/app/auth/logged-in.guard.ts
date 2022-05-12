ad096a9abba2ddda94007fbd6237488268043731
pavansarala <32029960+pavansarala@users.noreply.github.com>
2022-05-12 16:15:16 +0530

    Add files via upload

---
+++ angular/src/app/auth/logged-in.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Route, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggedInGuard implements CanActivate {
+
+  constructor(
+    private router : Router
+  ){
+
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(localStorage.getItem('loggedIn')=="true"){
+        this.router.navigate(["/home"])
+      }
+    return true;
+  }
+  
+}
